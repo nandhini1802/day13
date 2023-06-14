0e15ffcec8d1c53eb210bab78efec64020104f87
Nandhini <125853244+nandhini1802@users.noreply.github.com>
2023-06-14 23:56:43 +0530

    Add files via upload

---
+++ day-13/script.js
@@ -0,0 +1,141 @@
+function labelcreate(tagname,attrname,attrvalue,content){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.innerHTML=content;
+    return ele;
+    }
+    
+    //line breaker
+    function linebreakers(tagname){
+    var ele=document.createElement(tagname);
+    return ele;
+    }
+    //input elements
+    function inputele(tagname,attrname,attrvalue,attrname1,attrvalue1,attrname2,attrvalue2){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.setAttribute(attrname1,attrvalue1);
+    ele.setAttribute(attrname2,attrvalue2);
+    return ele;
+    }
+var Firstname=labelcreate("label","for","First Name","First Name");
+var br1=linebreakers("br");
+var Firstnameinput=inputele("input","type","First Name", "Name", "First Name","id","First Name");
+var br2=linebreakers("br");
+document.body.append(Firstname,br1,Firstnameinput,br2);
+
+
+
+function labelcreate(tagname,attrname,attrvalue,content){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.innerHTML=content;
+    return ele;
+    }
+    
+    //line breaker
+    function linebreakers(tagname){
+    var ele=document.createElement(tagname);
+    return ele;
+    }
+    //input elements
+    function inputele(tagname,attrname,attrvalue,attrname1,attrvalue1,attrname2,attrvalue2){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.setAttribute(attrname1,attrvalue1);
+    ele.setAttribute(attrname2,attrvalue2);
+    return ele;
+    }
+var Middlename=labelcreate("label","for","Middle Name","Middle Name");
+var br1=linebreakers("br");
+var Middlenameinput=inputele("input","type","Middle Name", "Name", "MiddleName","id","Middle Name");
+var br2=linebreakers("br");
+document.body.append(Middlename,br1,Middlenameinput,br2);
+
+
+
+function labelcreate(tagname,attrname,attrvalue,content){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.innerHTML=content;
+    return ele;
+    }
+    
+    //line breaker
+    function linebreakers(tagname){
+    var ele=document.createElement(tagname);
+    return ele;
+    }
+    //input elements
+    function inputele(tagname,attrname,attrvalue,attrname1,attrvalue1,attrname2,attrvalue2){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.setAttribute(attrname1,attrvalue1);
+    ele.setAttribute(attrname2,attrvalue2);
+    return ele;
+    }
+var Lastname=labelcreate("label","for","Last Name","Last Name");
+var br1=linebreakers("br");
+var Lastnameinput=inputele("input","type","Last Name", "Name", "Last Name","id","Last Name");
+var br2=linebreakers("br");
+document.body.append(Lastname,br1,Lastnameinput,br2);
+
+
+
+function labelcreate(tagname,attrname,attrvalue,content){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.innerHTML=content;
+    return ele;
+    }
+    
+    //line breaker
+    function linebreakers(tagname){
+    var ele=document.createElement(tagname);
+    return ele;
+    }
+    //input elements
+    function inputele(tagname,attrname,attrvalue,attrname1,attrvalue1,attrname2,attrvalue2){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.setAttribute(attrname1,attrvalue1);
+    ele.setAttribute(attrname2,attrvalue2);
+
+    return ele;
+    }
+var email=labelcreate("label","for","email","Email");
+var br1=linebreakers("br");
+var emailinput=inputele("input","type","email", "name", "email","id","email");
+var br2=linebreakers("br");
+document.body.append(email,br1,emailinput,br2);
+
+
+
+function labelcreate(tagname,attrname,attrvalue,content){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.innerHTML=content;
+    return ele;
+    }
+    
+    //line breaker
+    function linebreakers(tagname){
+    var ele=document.createElement(tagname);
+    return ele;
+    }
+    //input elements
+    function inputele(tagname,attrname,attrvalue,attrname1,attrvalue1,attrname2,attrvalue2){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.setAttribute(attrname1,attrvalue1);
+    ele.setAttribute(attrname2,attrvalue2);
+    return ele;
+    }
+
+var Password=labelcreate("label","for","Password","Password");
+var br1=linebreakers("br");
+var Passwordnameinput=inputele("input","type","Password", "Name", "Password","id","Password");
+var br2=linebreakers("br");
+document.body.append(Password,br1,Passwordnameinput,br2);
+
+
